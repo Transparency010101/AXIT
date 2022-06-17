d7251384d35440c895c9ce2a6896d3f632f3afbe
Transparency010101 <ossys1010110@gmail.com>
2022-06-17 04:20:27 +0300

    сделал axit. нужно адаптировать

--- src/js/source/menu-burger.js
+++ src/js/source/menu-burger.js
@@ -1,8 +1,10 @@
-const headerBurger = document.querySelector(".header__burger");
-const headerMenu = document.querySelector(".header__menu");
+if (Boolean(document.querySelector(".header__burger"))) {
+    const headerBurger = document.querySelector(".header__burger");
+    const headerMenu = document.querySelector(".header__menu");
 
-headerBurger.addEventListener("click", function (e) {
-    headerBurger.classList.toggle("active");
-    headerMenu.classList.toggle("active");
-    document.body.classList.toggle("lock");
-});
+    headerBurger.addEventListener("click", function (e) {
+        headerBurger.classList.toggle("active");
+        headerMenu.classList.toggle("active");
+        document.body.classList.toggle("lock");
+    });
+}
